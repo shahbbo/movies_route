73dfce36de4ae74a29b38099cb129a0ca9769800
soo597 <129106633+soo20@users.noreply.github.com>
2025-01-30 20:49:03 +0200

    resolve the confilct in text manager

--- lib/core/resources/text_manager.dart
+++ lib/core/resources/text_manager.dart
@@ -1,104 +1,81 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-import 'asset_manager.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'color_manager.dart';
 
 class FontManager {
   static TextStyle robotoBold24White = GoogleFonts.roboto(
-    color: ColorManager.primaryWhiteColor,
-    fontSize: 24,
-    fontWeight: FontWeight.bold
-  );
+      color: ColorManager.primaryWhiteColor,
+      fontSize: 24,
+      fontWeight: FontWeight.bold);
   static TextStyle robotoBold36White = GoogleFonts.roboto(
-    color: ColorManager.primaryWhiteColor,
-    fontSize: 36,
-    fontWeight: FontWeight.bold
-  );
+      color: ColorManager.primaryWhiteColor,
+      fontSize: 36,
+      fontWeight: FontWeight.bold);
   static TextStyle robotoBold20White = GoogleFonts.roboto(
-    color: ColorManager.primaryWhiteColor,
-    fontSize: 20,
-    fontWeight: FontWeight.bold
-  );
+      color: ColorManager.primaryWhiteColor,
+      fontSize: 20,
+      fontWeight: FontWeight.bold);
   static TextStyle robotoRegular16White = GoogleFonts.roboto(
-    color: ColorManager.primaryWhiteColor,
-    fontSize: 16,
-    fontWeight: FontWeight.w400
-  );
+      color: ColorManager.primaryWhiteColor,
+      fontSize: 16,
+      fontWeight: FontWeight.w400);
   static TextStyle robotoRegular14Yellow = GoogleFonts.roboto(
-    color: ColorManager.yellowColor,
-    fontSize: 14,
-    fontWeight: FontWeight.w400
-  );
+      color: ColorManager.yellowColor,
+      fontSize: 14,
+      fontWeight: FontWeight.w400);
   static TextStyle robotoBlack14Yellow = GoogleFonts.roboto(
-    color: ColorManager.yellowColor,
-    fontSize: 14,
-    fontWeight: FontWeight.w900
-  );
+      color: ColorManager.yellowColor,
+      fontSize: 14,
+      fontWeight: FontWeight.w900);
   static TextStyle robotoRegular16Yellow = GoogleFonts.roboto(
-    color: ColorManager.yellowColor,
-    fontSize: 16,
-    fontWeight: FontWeight.w400
-  );
+      color: ColorManager.yellowColor,
+      fontSize: 16,
+      fontWeight: FontWeight.w400);
   static TextStyle robotoRegular14White = GoogleFonts.roboto(
-    color: ColorManager.primaryWhiteColor,
-    fontSize: 14,
-    fontWeight: FontWeight.w400
-  );
+      color: ColorManager.primaryWhiteColor,
+      fontSize: 14,
+      fontWeight: FontWeight.w400);
   static TextStyle robotoRegular20White = GoogleFonts.roboto(
-    color: ColorManager.primaryWhiteColor,
-    fontSize: 20,
-    fontWeight: FontWeight.w400
-  );
+      color: ColorManager.primaryWhiteColor,
+      fontSize: 20,
+      fontWeight: FontWeight.w400);
   static TextStyle robotoRegular20Black = GoogleFonts.roboto(
-    color: ColorManager.blackColor,
-    fontSize: 20,
-    fontWeight: FontWeight.w400
-  );
+      color: ColorManager.blackColor,
+      fontSize: 20,
+      fontWeight: FontWeight.w400);
   static TextStyle robotoRegular16Black = GoogleFonts.roboto(
-    color: ColorManager.blackColor,
-    fontSize: 16,
-    fontWeight: FontWeight.w400
-  );
+      color: ColorManager.blackColor,
+      fontSize: 16,
+      fontWeight: FontWeight.w400);
   static TextStyle interMedium36White = GoogleFonts.inter(
-    color: ColorManager.primaryWhiteColor,
-    fontSize: 36,
-    fontWeight: FontWeight.w500
-  );
+      color: ColorManager.primaryWhiteColor,
+      fontSize: 36,
+      fontWeight: FontWeight.w500);
   static TextStyle interBold24White = GoogleFonts.inter(
-    color: ColorManager.primaryWhiteColor,
-    fontSize: 24,
-    fontWeight: FontWeight.bold
-  );
+      color: ColorManager.primaryWhiteColor,
+      fontSize: 24,
+      fontWeight: FontWeight.bold);
   static TextStyle interBold20Black = GoogleFonts.inter(
-    color: ColorManager.blackColor,
-    fontSize: 20,
-    fontWeight: FontWeight.bold
-  );
+      color: ColorManager.blackColor,
+      fontSize: 20,
+      fontWeight: FontWeight.bold);
   static TextStyle interBold20Yellow = GoogleFonts.inter(
-    color: ColorManager.yellowColor,
-    fontSize: 20,
-    fontWeight: FontWeight.bold
-  );
+      color: ColorManager.yellowColor,
+      fontSize: 20,
+      fontWeight: FontWeight.bold);
   static TextStyle interSemiBold20Yellow = GoogleFonts.inter(
-    color: ColorManager.yellowColor,
-    fontSize: 20,
-    fontWeight: FontWeight.w600
-  );
+      color: ColorManager.yellowColor,
+      fontSize: 20,
+      fontWeight: FontWeight.w600);
   static TextStyle interRegular20White = GoogleFonts.inter(
-    color: ColorManager.lowWhiteColor,
-    fontSize: 20,
-    fontWeight: FontWeight.w400
-  );
+      color: ColorManager.lowWhiteColor,
+      fontSize: 20,
+      fontWeight: FontWeight.w400);
   static TextStyle interSemiBold20Black = GoogleFonts.inter(
-    color: ColorManager.blackColor,
-    fontSize: 20,
-    fontWeight: FontWeight.w600
-  );
-
-}
-
+      color: ColorManager.blackColor,
+      fontSize: 20,
+      fontWeight: FontWeight.w600);
   static const String goldmanFont = "Goldman";
   static const String gothicA1 = "GothicA1";
   static TextStyle gothic60018() {
