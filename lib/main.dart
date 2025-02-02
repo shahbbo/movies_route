e8703a009515eb6b18da9ab7bb359da73b03f022
Mahmoud Shahbo <154024652+shahbbo@users.noreply.github.com>
2025-02-02 16:52:59 +0200

    edit profile by bebo

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       home: FirstOnboardPage(),
-      initialRoute: EditProfileScren.routeName,
+      initialRoute: SplashScreen.routeName,
       routes: {
         SplashScreen.routeName: (context) => SplashScreen(),
         FirstOnboardPage.route: (context) => FirstOnboardPage(),
