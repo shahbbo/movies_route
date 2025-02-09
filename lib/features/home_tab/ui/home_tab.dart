eec9cade42decf1825c037ed043176d910a7cb2d
Mahmoud Shahbo <m.shahbbo@icloud.com>
2025-02-09 02:52:55 +0200

    half of home tab ui & logic

--- lib/features/home_tab/ui/home_tab.dart
+++ lib/features/home_tab/ui/home_tab.dart
@@ -1,11 +1,9 @@
 import 'package:cached_network_image/cached_network_image.dart';
-import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_projects/core/customWidgets/MovieItem.dart';
-import 'package:flutter_projects/core/resources/asset_manager.dart';
 import 'package:flutter_projects/core/resources/color_manager.dart';
 import 'package:flutter_projects/features/home_tab/logic/home_tab_cubit.dart';
+import 'package:flutter_projects/features/home_tab/ui/widgets/movies_carousel_slider.dart';
 import '../data/model/MoviesListModel.dart';
 
 class HomeTab extends StatelessWidget {
@@ -18,8 +16,6 @@ class HomeTab extends StatelessWidget {
       builder: (context, state) {
         Size size = MediaQuery.sizeOf(context);
         final cubit = HomeTabCubit.get(context);
-        final List<Movies>? movies = cubit.moviesListModel?.data?.movies;
-
         return Column(
           children: [
             Container(
@@ -46,41 +42,7 @@ class HomeTab extends StatelessWidget {
                     stops: [0.0, 0.2, 0.5, 0.8,0.9, 1.0],
                   ),
                 ),
-                child: Column(
-                  children: [
-                    Image.asset(
-                      ImageAssets.availableNow,
-                      width: 200,
-                    ),
-                    CarouselSlider.builder(
-                      itemCount: movies?.length,
-                      itemBuilder: (context, index, realIndex) {
-                        return MovieItem(
-                          title: movies![index].title!,
-                          rating: movies[index].rating!,
-                          image: movies[index].largeCoverImage!,
-                        );
-                      },
-                      options: CarouselOptions(
-                        onPageChanged: (index, reason) {
-                          cubit.setHomeBackgroundImage(movies![index].largeCoverImage!);
-                        },
-                        height: 330,
-                        viewportFraction: 0.5,
-                        enlargeFactor: 0.30,
-                        enableInfiniteScroll: true,
-                        autoPlay: true,
-                        autoPlayCurve: Curves.slowMiddle,
-                        enlargeCenterPage: true,
-                        scrollDirection: Axis.horizontal,
-                      ),
-                    ),
-                    Image.asset(
-                      ImageAssets.watchNow,
-                      width: 200,
-                    ),
-                  ],
-                ),
+                child: MoviesCarouselSlider(),
               ),
             ),
           ],
